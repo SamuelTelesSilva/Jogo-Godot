075d21b1b496d41857f4f47ec1f32a73f2bad2e9
SamuelTelesSilva <samuel.silva@uni9.edu.br>
2020-03-15 00:45:03 -0300

    Programando/animação/movimentos

--- Scripts/cacadora.gd
+++ Scripts/cacadora.gd
@@ -12,17 +12,14 @@ var estaVivo = true
 
 func _ready():
 	set_process_input(true)
-	pass 
+	pass
 
 func _physics_process(delta):
 	linear_velocidade.y += gravidade * delta
 	
 	if estaVivo:
-		
 		if is_on_floor():
-			
 			linear_velocidade.x = 0
-			
 			if pulo:
 				linear_velocidade.y = forca_pulo
 		elif movimento:
@@ -37,3 +34,35 @@ func _physics_process(delta):
 			pass
 		
 	linear_velocidade = move_and_slide(linear_velocidade, Vector2(0, -1))
+	play_animacao()
+func _input(event):
+	
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			movimento = true and is_on_floor()
+			pulo = true
+		else:
+			movimento = false
+			
+func play_animacao():
+	
+	if estaVivo:
+		if is_on_floor():
+			$animacao.play("idle")
+		else:
+			if linear_velocidade.y > 0:
+				$animacao.play("idle")
+			elif linear_velocidade.y < 0:
+				$animacao.play("jump")
+	
+	else:
+		$animacao.play("dead")
+	
+	
+	
+	
+	
+	
+	
+	
+	
